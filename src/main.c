8a43c2a5f1adc5ba1ad8caba5318cd19a911c579
Eric <compaq963@gmail.com>
2025-12-02 21:27:22 -0800

    Move logging level back to INFO

--- src/main.c
+++ src/main.c
@@ -113,7 +113,7 @@ int main(int argc, char *argv[]) {
     pthread_mutexattr_destroy(&attr);
 
     log_set_lock(console_lock_function, &g_console_mutex);
-    log_set_level(LOG_DEBUG);
+    log_set_level(LOG_INFO);
 
     memset(&config, 0, sizeof(AppConfig));
 
