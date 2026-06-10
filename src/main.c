f824380d1e00827d90a47381984b65fa7f58f26a
Eric <compaq963@gmail.com>
2026-06-10 01:16:19 -0700

    Hide closing messages for uninitialized modules

--- src/main.c
+++ src/main.c
@@ -216,17 +216,20 @@ cleanup:
 
     bool final_ok = !app.stats.error_occurred;
 
-    if (app.pipeline_mode == PIPELINE_MODE_BUFFERED_INPUT) {
-        log_info("Stopping %s live source capture...", config.input.type_name);
-    } else {
-        log_info("Finished %s file processing.", config.input.type_name);
+    if (app.module.input_api) {
+        if (app.pipeline_mode == PIPELINE_MODE_BUFFERED_INPUT) {
+            log_info("Stopping %s live source capture...", config.input.type_name);
+        } else {
+            log_info("Finished %s file processing.", config.input.type_name);
+        }
+        log_info("Closing %s input module...", config.input.type_name);
+        close_input_source(&config, &app);
     }
 
-    log_info("Closing %s input module...", config.input.type_name);
-    close_input_source(&config, &app);
-
-    log_info("Closing %s output module...", config.output.module_name);
-    close_output_module(&config, &app);
+    if (app.module.output_api) {
+        log_info("Closing %s output module...", config.output.module_name);
+        close_output_module(&config, &app);
+    }
 
     if (resources_initialized) {
         print_final_summary(&config, &app, final_ok);
